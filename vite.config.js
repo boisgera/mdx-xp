8e35dffeccef9ac719c5e7c1be7babce44290f9d
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-04-22 15:47:08 +0200

    Snapshot

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import mdx from '@mdx-js/rollup'
+import { defineConfig } from 'vite';
+import mdx from '@mdx-js/rollup';
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax";
 import civet from '@danielx/civet/vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
-    { enforce: 'pre', ...mdx() },
+    { 
+      enforce: 'pre', 
+      ...mdx({
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeMathjax],
+    }) 
+  },
     civet({ "ts": "preserve" }), 
     react()
   ],
